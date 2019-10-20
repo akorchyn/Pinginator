9da5f1e37c66cd5678ca4ce3a623eb623b5c774c
akorchyn <akorchyn@student.unit.ua>
2019-10-20 15:01:24 +0300

    Refactored code. Added mongodb.

--- bot.py
+++ bot.py
@@ -1,33 +1,23 @@
 import telebot
-import os.path
-import signal
-import sys
+import pymongo
+import os
 
-TOKEN = ""
+if 'BOT_TOKEN' not in os.environ:
+    print("Please set env variable: BOT_TOKEN")
+    exit(0)
 
+TOKEN = os.environ['BOT_TOKEN']
+LOGIN = os.environ.get('BOT_LOGIN', "pinginatorbot")
+NAME = os.environ.get('BOT_NAME', "Pinginator")
+client = pymongo.MongoClient()
+groups = client.botDb
 bot = telebot.TeleBot(TOKEN)
-NAME = "pinginatorbot"
-users = dict()
 
 
-def signal_handler(signal, frame):
-    for group in users:
-        f = open(group, 'w')
-        for user in users[group]:
-            f.write(user + '\n')
-        f.close()
-    sys.exit(0)
-
-
-def check_and_upload(group_id):
-    if users.get(group_id) is not None:
-        return
-    users[group_id] = set()
-    if os.path.isfile(group_id):
-        f = open(group_id, 'r')
-        temp = f.read().splitlines()
-        for line in temp:
-            users[group_id].add(line)
+def insert_user(message):
+    if message.chat.type != 'private':
+        groups[str(message.chat.id)].update_one({'name': message.from_user.username},
+                                                {'$set': {'name': message.from_user.username}}, True)
 
 
 @bot.message_handler(commands=['ping'])
@@ -38,47 +28,39 @@ def text_handler(message):
         bot.send_message(chat_id, 'I AM A GROUP BOT :C')
     else:
         text = ''
-        check_and_upload(str(message.chat.id))
-        for a in users[str(message.chat.id)]:
-            if a != message.from_user.username and a != NAME:
-                text += '@' + a + ' '
+        users = groups[str(message.chat.id)].find({})
+        for user in users:
+            if user['name'] != message.from_user.username and user['name'] != NAME:
+                text += '@' + user['name'] + ' '
         if len(text) > 0:
             bot.send_message(message.chat.id, text)
+    insert_user(message)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'Hello ' + message.from_user.first_name + ', my name is Pinginator.\n'
+    bot.send_message(message.chat.id, 'Hello ' + message.from_user.first_name + ', my name is ' + NAME + '.\n'
                                       'I will ping all user in group, if you use /ping\n' +
                                       ('I am useless in private chat. I am group bot.' if message.chat.type == 'private' else ''))
+    insert_user(message)
 
 
 @bot.message_handler(content_types=["new_chat_members"])
 def handler_new_member(message):
-    key = str(message.chat.id)
-    check_and_upload(key)
-    users[key].add(message.new_chat_member.username)
+    groups[str(message.chat.id)].insert_one({'name': message.new_chat_member.username})
 
 
 @bot.message_handler(content_types=["left_chat_member"])
 def handler_left_member(message):
-    key = str(message.chat.id)
-    check_and_upload(key)
-    if message.left_chat_member.username not in users[key]:
-        return
-    users[key].remove(message.left_chat_member.username)
+    groups[str(message.chat.id)].delete_one({'name': message.left_chat_member.username})
 
 
 @bot.message_handler(content_types=["text"])
 def handler_text(message):
-    key = str(message.chat.id)
-    check_and_upload(key)
     if '@' + NAME in message.text:
         bot.send_message(message.chat.id, '@' + message.from_user.username)
-    if message.from_user.username not in users[key]:
-        users[key].add(message.from_user.username)
+    insert_user(message)
 
 
-signal.signal(signal.SIGINT, signal_handler)
 bot.polling()
 
