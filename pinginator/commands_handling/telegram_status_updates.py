82dd1faaa7bacd28dad922c8c65fc007e9665d25
artur.yurii.korchynskyi@gmail.com <artur.yurii.korchynskyi@gmail.com>
2020-07-31 23:29:35 +0300

    Fixed migration to supergroup
    Signed-off-by: artur.yurii.korchynskyi@gmail.com <artur.yurii.korchynskyi@gmail.com>

--- pinginator/commands_handling/telegram_status_updates.py
+++ pinginator/commands_handling/telegram_status_updates.py
@@ -11,11 +11,20 @@ def insert_user(*_):
 
 
 def remove_user(update: Update, context: CallbackContext):
+    if update.effective_message.left_chat_member.id == context.bot.id:
+        context.bot_data['db'].remove_group(update.effective_chat.id)
+        return
     context.bot_data['db'].remove_user(update.effective_chat.id,
                                        User(update.effective_message.left_chat_member.id))
 
 
-TELEGRAM_HANDLES = [MessageHandler(Filters.status_update & (~Filters.status_update.left_chat_member),
-                                   callback=insert_user),
-                    MessageHandler(Filters.status_update.left_chat_member, remove_user)
+def group_upgraded_to_supergroup(update: Update, context: CallbackContext):
+    if update.effective_message.migrate_to_chat_id is not None:
+        context.bot_data['db'].migrate_group(update.effective_chat.id, update.effective_message.migrate_to_chat_id)
+
+
+TELEGRAM_HANDLES = [MessageHandler(Filters.status_update & (~Filters.status_update.left_chat_member)
+                                   & (~Filters.status_update.migrate), callback=insert_user),
+                    MessageHandler(Filters.status_update.left_chat_member, remove_user),
+                    MessageHandler(Filters.status_update.migrate, group_upgraded_to_supergroup)
                     ]
